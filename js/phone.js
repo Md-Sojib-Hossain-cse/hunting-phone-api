949ce98da17110763ac7380ba3424a4b755a2cbf
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-29 18:13:39 +0600

    adding all initial things

--- js/phone.js
+++ js/phone.js
@@ -1,35 +1,13 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
+const loadData = async () =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await response.json();
     const phones = data.data;
     // console.log(phones)
     displayPhones(phones);
 }
 
-
 const displayPhones = phones => {
-    // 1. where to append
-    const phoneContainer = document.getElementById('phonesContainer');
-    phones.forEach(phone => {
-        //2. create a div
-        const div = document.createElement('div');
-        div.classList = `card w-96 bg-base-100 shadow-xl`;
-        // 3. add inner text
-        div.innerHTML = `
-        <figure><img src="${phone.image}" alt="Shoes" /></figure>
-        <div class="card-body">
-            <h2 class="card-title">${phone.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-            </div>
-        </div>
-        `
-        // 4. append child
-        phoneContainer.appendChild(div);
-        console.log(phone)
-    });
+    console.log(phones)
 }
 
-
-loadPhone();
+loadData();
